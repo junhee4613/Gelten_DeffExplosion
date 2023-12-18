608a1006faad8803d7f260a4309b562e4a45594f
junhee4613 <junhee000123@naver.com>
2023-12-18 15:17:48 +0900

    Feat : Managers, SoundManager

---
+++ Gelten_DeffExplosion_Unity/Assets/02.Scripts/Managers/Managers.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;
+    static Managers Instance { get { Init(); return s_instance; } }
+    // Start is called before the first frame update
+    public static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+    SoundManager _sound = new SoundManager();
+
+    public static SoundManager Sound { get { return Instance._sound; } }
+}
